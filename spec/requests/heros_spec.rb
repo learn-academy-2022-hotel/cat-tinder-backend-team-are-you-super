c8223bad89b61235a02eab088d899ab6ad8eab9b
monicaarganda <argandamonica1@gmail.com>
2023-01-31 10:37:40 -0800

    Api-Endpoint Complete

--- spec/requests/heros_spec.rb
+++ spec/requests/heros_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Heros", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of heros" do
+      Hero.create(
+        name: 'Iron Man',
+        super_power: 'Self-built powered armor that gives super strength, Durability, and Flight',
+        enjoys: 'Skiing, Parachuting, and Hang-gliding',
+        image: 'https://media.vanityfair.com/photos/5a1c7f8c7eee1d789d984783/4:3/w_1776,h_1332,c_limit/marvel-iron-man-lede.jpg'
+      )
+      get '/heros'
+
+      hero = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(hero.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a hero" do
+      hero_params = {
+        hero: {
+          name: 'Iron Man',
+          super_power: 'Self-built powered armor that gives super strength, Durability, and Flight',
+          enjoys: 'Skiing, Parachuting, and Hang-gliding',
+          image: 'https://media.vanityfair.com/photos/5a1c7f8c7eee1d789d984783/4:3/w_1776,h_1332,c_limit/marvel-iron-man-lede.jpg'
+        }
+      }
+      post '/heros', params: hero_params
+
+      expect(response).to have_http_status(200)
+
+      hero = Hero.first
+
+      expect(hero.name).to eq 'Iron Man'
+    end
   end
 end
